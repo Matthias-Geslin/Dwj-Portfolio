caf7fdab181a8a345e574eb5bb4becf6c31dd6e2
Matthias <m.geslin974@gmail.com>
2019-05-27 18:54:18 +0200

    Navbar object edit -- selectors events

--- js/Navbar.js
+++ js/Navbar.js
@@ -1,5 +1,44 @@
 'use strict';
 
-var Navbar = function () {
+var NavBar = function () {
+    this.navSelector();
+    this.homeInSelector();
+    this.portfolioInSelector();
+    this.contactInSelector();
+};
 
+NavBar.prototype.navSelector = function () {
+    const homeSelector = document.getElementById("home");
+    const homeBoxSelector = document.getElementById("home-box");
+
+    const portfolioSelector = document.getElementById("portfolio");
+    const portfolioBoxSelector = document.getElementById("portfolio-box");
+
+    const contactSelector = document.getElementById("contact");
+    const contactBoxSelector = document.getElementById("contact-box");
+
+
+    this.homeInSelector = function () {
+        homeSelector.addEventListener('click', function (e) {
+            homeBoxSelector.classList.remove('disabled');
+            portfolioBoxSelector.classList.add('disabled');
+            contactBoxSelector.classList.add('disabled');
+        });
+    };
+
+    this.portfolioInSelector = function () {
+        portfolioSelector.addEventListener('click', function () {
+            homeBoxSelector.classList.add('disabled');
+            portfolioBoxSelector.classList.remove('disabled');
+            contactBoxSelector.classList.add('disabled');
+        });
+    };
+
+    this.contactInSelector = function () {
+        contactSelector.addEventListener('click', function () {
+            homeBoxSelector.classList.add('disabled');
+            portfolioBoxSelector.classList.add('disabled');
+            contactBoxSelector.classList.remove('disabled');
+        });
+    };
 };
